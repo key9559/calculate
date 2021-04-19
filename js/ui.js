28374d12c4b48858c2bf034cb575cb0f78aede42
key9559 <key9559@naver.com>
2021-04-19 15:24:16 +0900

    글자 저장

--- js/ui.js
+++ js/ui.js
@@ -3,14 +3,46 @@ const section = document.querySelector(".section");
 
 section.style.height = window.innerHeight + 'px';
 
+// btn select
 const btnWrap = document.querySelectorAll('.btn-wrap');
+
+// roof btn index
 for (i = 0; i < btnWrap.length; i++) {
   
+  // btn and result select
   const btns = document.querySelector(".btn-wrap:nth-child(" + [i + 1] + ") .btn");
-  const result = document.querySelector(".result");
+  const result = document.getElementById("result");
 
+  // btn click event
   btns.addEventListener("click", function () {
-    const textSave = this.innerText;
-    console.log(textSave);
+    const savePrev = result.innerText;
+    
+    // icon button return false
+    if(btns.classList.contains('icon')) {
+      return false;
+    } else {
+      //btn text save
+      const textSave = this.innerText;
+      // console.log(textSave);
+  
+      // btn text result result
+      result.innerText = savePrev + textSave;
+    }
   })
 }
+
+// icno button select
+let clearBtn = document.getElementById('clear');
+let equalBtn = document.getElementById('equal');
+
+// clear click == reset
+clearBtn.addEventListener('click', function () {
+  result.innerText = '';
+})
+
+// equal click == calculate
+equalBtn.addEventListener('click', function () {
+  const saveCalc = result.innerText;
+
+  console.log(saveCalc, typeof saveCalc); // string
+})
