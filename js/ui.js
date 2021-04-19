ae300dbdd2b8a44b3faf8dbb2fadfd6a521db576
key9559 <key9559@naver.com>
2021-04-19 12:52:14 +0900

    글자 저장성공

--- js/ui.js
+++ js/ui.js
@@ -2,3 +2,15 @@
 const section = document.querySelector(".section");
 
 section.style.height = window.innerHeight + 'px';
+
+const btnWrap = document.querySelectorAll('.btn-wrap');
+for (i = 0; i < btnWrap.length; i++) {
+  
+  const btns = document.querySelector(".btn-wrap:nth-child(" + [i + 1] + ") .btn");
+  const result = document.querySelector(".result");
+
+  btns.addEventListener("click", function () {
+    const textSave = this.innerText;
+    console.log(textSave);
+  })
+}
